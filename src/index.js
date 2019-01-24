2c60dbfbfdc7ad4192f2eddff2b8ea8f76fc2125
kiquetal <kiquetal@gmail.com>
2019-01-24 00:59:50 -0300

    adding new lines

--- src/index.js
+++ src/index.js
@@ -3,19 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Author from './Author';
 import * as serviceWorker from './serviceWorker';
-
+import {shuffle,sample} from 'underscore';
 
 const authors=[{
     name:"Mark Twain",
     imageURL:'images/authors/marktwain.jpg',
     imageSource:'Wikimedia Commons',
     books:['The adventures of Huckley Fin','Seconds books']
-}];
+},{
+    name:"Stephen King",
+    imageURL:"images/authors/stephenking.jpg",
+    imageSource:'Wikimedia Commons',
+    books:['The shinning','Carrie','The call']
+},
+    {
+        name:'Chuck Palahniuk',
+        imageURL:'images/authors/chuckpalahniuk.jpg',
+        imageSource:'Wikimedia Commons',
+        books:['Damn','Fight CLub']
+    },
+    {
+        name:"George Romero",
+        imageURL:'images/authors/georgeromero.jpg',
+        imageSource:'Wiki media',
+        books:['zombie 1','zombie 2']
+    }];
+
+function getTurnData(authors)
+{
+    const allBooks=authors.reduce((p,c,i) =>{
+        return p.concat(c.books)
+},[]);
+    const fourRandomBooks=shuffle(allBooks).slice(0,4)
+    const answer=sample(fourRandomBooks)
+
+return {
+        books:fourRandomBooks,
+        author:authors.find((author)=> author.books.some((title)=> title===answer))
+    }
+}
+
 const state={
-    turnData:{
-    author:authors[0],
-    books:authors[0].books
-}}
+    turnData: getTurnData(authors)
+}
 
 
 
